ae695fe4a44b2eb5a2a4bc2ff926e90f243eba79
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2024-01-05 14:46:10 -0500

    Retiro apikey

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.restaurantxDEPN',
   appName: 'myFavoriteRestaurant',
   webDir: 'www',
   server: {
